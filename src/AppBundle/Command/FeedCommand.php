37e9046235623c17a07fa8356cda2afa190fdfac
gabriel <pour.blaireaux@yahoo.fr>
2017-04-14 11:06:55 +0200

    initial commit

--- src/AppBundle/Command/FeedCommand.php
+++ src/AppBundle/Command/FeedCommand.php
@@ -5,6 +5,7 @@ namespace AppBundle\Command;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -18,7 +19,8 @@ class FeedCommand extends ContainerAwareCommand
     {
         $this
             ->setName('app:feed')
-            ->addArgument('merchantCode', InputArgument::REQUIRED, 'The merchant code')
+            ->addOption('all', 'a',InputOption::VALUE_NONE, 'the feed mode')
+            ->addArgument('merchantCode', InputArgument::OPTIONAL, 'The merchant code')
             ->setDescription('Reads the given merchant\'s data feed and create or updates offers.')
             ->setHelp(<<<EOT
 La commande app:feed permet de récupérer les offres des sites marchands affiliés d'après leur flux de données :
@@ -30,14 +32,68 @@ EOT
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        // récupérer l'option
+        $getAll = $input->getOption('all');
+
         // Récupérer le service "Feed Reader"
+        $reader = $this->getContainer()->get('app.feed.reader');
 
         // Récupérer le service "Doctrine Entity Manager"
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
 
-        // Trouver le marchand d'après le code passé en argument de la commande
+        // repo
+        $code = $input->getArgument('merchantCode');
+        $repo = $em->getRepository('AppBundle:Merchant');
+
+        // Trouver unmarchand d'après le code passé en argument de la commande
+        if( !$getAll) {
+
+            $code = $input->getArgument('merchantCode');
+
+            if( $code == null) {
+                $output->writeln('il faut préciser un code marchant');
+                return;
+            }
+
+            $merchants = $repo->findByCode($code);
+        }
+        // trouver tous les marchants
+        else  {
+            $merchants = $repo->findAll();
+        }
 
         // Utiliser le service "Feed Reader" pour récupérer les offres du flux du marchand
+        $result = 0;
+        foreach($merchants as $merchant) {
+            $n = $merchant->getName();
+            $output->writeln("Fetching merchant $n");
+            $result += $reader->read($merchant);
+        }
+
+        // mail
+        $message = \Swift_Message::newInstance()
+            ->setTo('gabriel.daudin@nantes.imie.fr')
+            ->setFrom('gabriel.daudin@nantes.imie.fr')
+            ->setSubject('Update done')
+
+            /*
+             * From a controller, next expression should be :
+             *
+             *  ->setBody($this->renderView(
+                'AppBundle:Email:contact.html.twig', [
+                    'result' =>  $result
+                ]
+            ), 'text/html');
+             */
+
+            ->setBody($this->getContainer()->get('templating')->render(
+                'AppBundle:Email:contact.html.twig', [
+                    'result' =>  $result
+                ]
+            ), 'text/html');
+        $sent = $this->getContainer()->get('mailer')->send($message);
 
         // Afficher (dans le terminal) le nombre d'offres créées ou mises à jour.
+        $output->writeln("$result offres mise(s) à jour");
     }
 }
--- src/AppBundle/Feed/Reader.php
+++ src/AppBundle/Feed/Reader.php
@@ -3,7 +3,10 @@
 namespace AppBundle\Feed;
 
 use AppBundle\Entity\Merchant;
+use AppBundle\Entity\Offer;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\Validator\Validator\RecursiveValidator;
 
 /**
  * Class Reader
@@ -17,15 +20,18 @@ class Reader
      */
     private $em;
 
+    /** @var  RecursiveValidator */
+    private $validator;
 
     /**
      * Constructor.
      *
      * @param EntityManagerInterface $entityManager
      */
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager,RecursiveValidator $validator)
     {
         $this->em = $entityManager;
+        $this->validator = $validator;
     }
 
     /**
@@ -38,25 +44,130 @@ class Reader
     public function read(Merchant $merchant)
     {
         // $count = 0;
+        $count = 0;
 
         // Lire le flux de données du marchand
+        $url = $merchant->getFeedUrl();
 
         // Convertir les données JSON en tableau
+        $ext = explode('.', $url);
+        $ext = end($ext);
+        switch($ext) {
+            case 'json':
+                $array = $this->jsonFluxToArray($url);
+                break;
+
+            case 'xml':
+                $array = $this->xmlFluxToArray($url);
+                break;
+
+            case 'csv':
+                $array = $this->csvFluxToArray($url);
+                break;
+
+            default:
+                throw new Exception("Extension de flux non valide : $ext");
+        }
+
+        // repo
+        $productRepo = $this->em->getRepository('AppBundle:Product');
+        $offerRepo = $this->em->getRepository('AppBundle:Offer');
 
         // Pour chaque couple de données "code ean / prix"
+        foreach($array as $data) {
 
             // Trouver le produit correspondant
+            $product = $productRepo->findOneByEanCode($data['ean_code']);
 
                 // Sinon passer à l'itération suivante
+                if( $product == null) {
+                    continue;
+                }
 
             // Trouver l'offre correspondant à ce produit et ce marchand
+            $offer = $offerRepo->findOneByProduct($product);
 
                 // Sinon créer l'offre
+                if( $offer == null) {
+                    $offer = new Offer();
+                    $offer->setMerchant($merchant);
+                    $offer->setProduct($product);
+                }
 
             // Mettre à jour le prix et la date de mise à jour de l'offre
+            $offer->setPrice($data['price']);
+            $offer->setUpdatedAt(new \DateTime());
 
             // Enregistrer l'offre et incrémenter le compteur d'offres
+            $errors = $this->validator->validate($offer);
+
+            if( count($errors) == 0) {
+                $this->em->persist($offer);
+                ++$count;
+            }
+        }
+
+        //flush
+        if($count > 0) {
+            $this->em->flush();
+        }
 
         // Renvoyer le nombre d'offres
+        return $count;
+    }
+
+    /**
+     * convert JSON flux to associative array
+     * @param string $url
+     * @return array
+     */
+    private function jsonFluxToArray($url){
+        $content = file_get_contents($url);
+        return json_decode($content, true);
+    }
+
+    /**
+     * convert CSV flux to associative array
+     * @param string $url
+     * @return array
+     */
+    private function csvFluxToArray($url){
+        $handle = fopen($url, 'r');
+
+        $array = array();
+        while (false !== $data = fgetcsv($handle, null, ';')) {
+            // $data = [<eandCode>, <price>];
+            $tmp = array();
+            $tmp['ean_code'] = intval($data[0]);
+            $tmp['price'] = floatval($data[1]);
+            array_push($array, $tmp);
+        }
+        fclose($handle);
+
+        return $array;
+    }
+
+    /**
+     * convert XML flux to associative array
+     * @param string $url
+     * @return array
+     */
+    private function xmlFluxToArray($url){
+        $dom = new \DOMDocument();
+        $dom->load($url);
+
+        $offers = $dom->getElementsByTagName('offer');
+
+        /** @var \DomElement $offer */
+        $array = array();
+        foreach ($offers as $offer) {
+            // public function \DomElement::getAttribute(string $name);
+            $tmp = array();
+            $tmp['ean_code'] = intval($offer->getAttribute('ean_code'));
+            $tmp['price'] = floatval($offer->getAttribute('price'));
+            array_push($array, $tmp);
+        }
+
+        return $array;
     }
 }
